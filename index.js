f65c5401ef83ade062a5ee991879b19a65485af8
Felipe Nogueira <felipejbass@gmail.com>
2018-10-17 23:59:15 -0300

    commiting release candidate
    commiting release candidate

--- index.js
+++ index.js
@@ -1,178 +1,172 @@
 const scrapeIt = require("scrape-it")
 var request = require('request');
 var cheerio = require('cheerio');
-
 const puppeteer = require('puppeteer');
-/*
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.playstation.com/en-us/explore/playstationnow/');
-  await page.screenshot({path: 'example.png'});
+var Q = require('q');
 
-  const handle = await page.evaluateHandle(() => ({window, document}));
-  const properties = await handle.getProperties();
-  const windowHandle = properties.get('window');
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
-  const documentHandle = properties.get('document');
-
-  console.log(properties);
-  //console.log(windowHandle);
-
-  await browser.close();
-})();*/
-
-function verificarVersao(endereco){
-  puppeteer.launch().then(async browser => {
-    try{
-      const page = await browser.newPage();
-      await page.goto(endereco);
-      console.log(endereco+" - "+ await page.evaluate(() => Ember.VERSION));
-    }catch(error){
-      console.log("Erro no site - "+endereco);
-    }
-  //await page.goto('https://www.playstation.com/en-us/explore/playstationnow/');
-  //console.log(await page.evaluate(() => Ember.VERSION));
-  await browser.close();
+//Set CSV header and path
+const csvWriter = createCsvWriter({
+    header: ['URL', 'Ember_version'],
+    path: './ember_checkerVers.csv'
 });
 
-}
 
 var urlSet = [];
 
 
+//Scrape a page and return URL from table 
 
+var getURLList = function(pageIndex) {
+    var deferred = Q.defer();
 
-
-/*scrapeIt("https://www.bharatdarshan.co.nz/", {
-  scriptJS:{listItem: "script"}
-}).then(({ data, response }) => {
-
-    //console.log(data);
-  
-})*/
-
-/*
-url = 'https://www.playstation.com/en-us/explore/playstationnow/';
-
-request(url, function(error, response, html){
-  //console.log(html);
-
-  var $ = cheerio.load(html);
-  console.log($('Ember'));
-});*/
-
-
-/*
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-
-url = 'https://www.playstation.com/en-us/explore/playstationnow/';
-
-request(url, function(error, response, html){
-  console.log(html);
-
-  const dom = new JSDOM(html, { runScripts: "dangerously",  resources: "usable" });
-
-
-    setTimeout(() => {
-      console.log("dom.window");
-      console.log(html);
-      
-      const emberVersion = dom.window.Ember && dom.window.Ember.VERSION ? dom.window.Ember.VERSION : 'Not Using Ember';
-
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-
-  }, 40000);*/
-
-  //var $ = cheerio.load(html);
-  //console.log($);
-//});
-
-
-//const jsdom = require("jsdom");
-//const { JSDOM } = jsdom;
-
-//const fetch = require('node-fetch');
-
-//fetch('https://www.playstation.com/en-us/explore/playstationnow/')
-//.then(res => res.text())
-//.then(body => {
-//    const dom = new JSDOM(body, { runScripts: "dangerously",  resources: "usable" });
-
-
-  //  setTimeout(() => {
-  //    console.log("dom.window");
-//      console.log(body);
-      
-      //const emberVersion = dom.window.Ember && dom.window.Ember.VERSION ? dom.window.Ember.VERSION : 'Not Using Ember';
-
-    /*console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-      console.log(`Ember Version = ${emberVersion}`);
-*/
-//  }, 10000);
-
-//});
-
-
-
-  var p1 = new Promise(
-    (resolve, reject) => {
-
-      var k=0;
-      for(var j=1;j<=12;j++)
-      {
-        
-        scrapeIt("https://domaintyper.com/top-websites/most-popular-websites-with-nz-domain/page/"+j, {
-          data: {
-            listItem: "tr", 
-            data:{
-                listItem:"td", data:{listItem:'td'}
+    scrapeIt("https://domaintyper.com/top-websites/most-popular-websites-with-nz-domain/page/" + pageIndex, {
+        data: {
+            listItem: "tr",
+            data: {
+                listItem: "td",
+                data: { listItem: 'td' }
             }
-          }
-        }).then(({ data, response }) => {
+        }
+    }).then(({ data, response }) => {
+        deferred.resolve(data)
+    });
 
-
-      //console.log(`Status Code: ${response.statusCode}`)
-    
-      for(var i=0;i<data.data.length;i++)
-      {
-
-          //console.log(urlSet);
-          urlSet.push("https://"+data.data[i].data[1]);
-          //console.log("https://"+data.data[i].data[1]);
-          //verificarVersao("https://"+data.data[i].data[1]);
-    
-      }
-
-      resolve(urlSet) 
-    }
-    
-  )
-        
+    return deferred.promise;
 }
 
-    });
+let func = [];
 
-  p1.then(
-    function(val) {
-      console.log(val.length);
-      (async function loop() {
-    for (let i = 0; i < 100; i++) {
-        //await new Promise(resolve => setTimeout(resolve, Math.random() * 10000));
-        await verificarVersao(val[i]);
-        console.log(val[i]);
+//Build an array of URL from each page 
+var scrapeURLList = function() {
+    var deferred = Q.defer();
+
+
+    for (var pIndex = 1; pIndex <= 10; pIndex++) {
+        func.push(getURLList(pIndex));
     }
-})();
 
+    Q.allSettled(func).then((results) => {
+        var values = [];
+        var urlSet = [];
+        console.log("Results received!!");
+        results.forEach(function(result) {
+            if (result.state === "fulfilled") {
+                values.push(result.value);
+
+            } else {
+                rejectedReq.push(result.reason);
+            }
+        });
+
+
+        for (var i = 0; i < values.length; i++) {
+
+            for (var j = 1; j < values[i].data.length; j++) {
+                urlSet.push(values[i].data[j].data[1])
+            }
+
+        }
+
+        deferred.resolve(urlSet)
+
+    }, (error) => {
+        console.log('Error allSettled ', error);
+        deferred.reject(urlSet)
+    })
+
+    console.log('End Await');
+
+    return deferred.promise;
+
+
+
+}
+
+scrapeURLList().then(urlLis => {
+
+
+    (async () => {
+
+        for (var i = 0; i < urlLis.length; i++) {
+
+            console.log(urlLis[i]);
+
+            await checkEmberVersion('http://' + urlLis[i]).then(input => {
+                console.log('input');
+                console.log(input);
+
+
+                var records = [
+                    [input.url, input.emberVer]
+                ];
+                csvWriter.writeRecords(records).then(() => {
+                    console.log('row recorded');
+                });
+
+
+            }, function(error) {
+                console.log('No Ember Found');
+                
+
+                var records = [
+                    [urlLis[i], 'None']
+                ];
+                csvWriter.writeRecords(records).then(() => {
+                    console.log('row recorded');
+                });
+            });
+
+        }
+
+
+
+    })()
+
+
+
+});
+
+
+
+function checkEmberVersion(addressURL) {
+
+    var deferred = Q.defer();
+    puppeteer.launch().then(async browser => {
+        try {
+            const page = await browser.newPage();
+            await page.goto(addressURL, { waitUntil: 'load', timeout: 0 });
+
+
+            try {
+                var emberVers = await page.evaluate(() => Ember.VERSION);
+
+                var checkedData = {
+                    url: addressURL,
+                    emberVer: emberVers
+                };
+
+
+
+                deferred.resolve(checkedData);
+            } catch (e) {
+
+                deferred.reject('an expection on page.evaluate ', e);
+            }
+
+
+
+            //console.log(checkedData);
+
+        } catch (error) {
+            console.log("Erro no site - " + error);
+            deferred.reject(error);
+        }
+
+        await browser.close();
     });
+
+    return deferred.promise;
+
+}
